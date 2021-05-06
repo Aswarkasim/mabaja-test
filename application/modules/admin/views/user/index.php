549fef83467043098dfb9232ae49ee10cca1ed1d
aswarkasim <aswarkasim@gmail.com>
2021-05-06 15:59:34 +0700

    aa

--- application/modules/admin/views/user/index.php
+++ application/modules/admin/views/user/index.php
@@ -29,7 +29,40 @@
                     <th width="200px">Tindakan</th>
                 </tr>
             </thead>
+            <tbody id="">
+                <?php $no = 1;
+                foreach ($user as $row) { ?>
+                    <tr>
+                        <td><?= $no ?></td>
+                        <td>
+                            <i class="fa fa-user <?= $row->is_read == 1 ? 'is-read' : 'not-read'; ?>"></i> <strong><?= $row->namalengkap ?></strong><br>
+                            <p><?= $row->email ?></p>
+                        </td>
 
+                        <td>
+
+                        </td>
+
+                        <td>
+
+                        </td>
+
+                        <td>
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-info"><i class="fa fa-cogs"></i></button>
+                                <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">
+                                    <span class="caret"></span>
+                                    <span class="sr-only">Toggle Dropdown</span>
+                                </button>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a class="tombol-hapus" href="<?= base_url($delete . $row->id_user)  ?>"><i class="fa fa-trash"></i> Hapus</a></li>
+                                </ul>
+                            </div>
+                        </td>
+                    </tr>
+                <?php $no++;
+                } ?>
+            </tbody>
         </table>
 
     </div>
