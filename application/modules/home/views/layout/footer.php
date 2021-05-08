a7e15ab64eeec697ff85af78153aab8b2dc278b6
aswarkasim <aswarkasim@gmail.com>
2021-05-08 07:52:33 +0700

    kecermatan reset perkelas

--- application/modules/home/views/layout/footer.php
+++ application/modules/home/views/layout/footer.php
@@ -24,7 +24,7 @@
 
 <footer class="footer">
   <div class="container">
-    <span class="text-muted">Place sticky footer content here.</span>
+    <span class="text-muted">Created by <a href="http://scrollupstudio.com" target="blank">ScrollUp Studio</a></span>
   </div>
 </footer>
 
