f33195df9b96d63316f96945f7dd93f89181d63d
aswarkasim <aswarkasim@gmail.com>
2021-10-06 07:20:28 +0700

    asc

--- application/modules/admin/models/Soal_model.php
+++ application/modules/admin/models/Soal_model.php
@@ -24,7 +24,8 @@ class Soal_model extends CI_Model
   {
     $this->db->select('*')
       ->from('tbl_simulasi')
-      ->where('id_mapel', $id_mapel);
+      ->where('id_mapel', $id_mapel)
+      ->orde_by('nama_simulasi', 'ASC');
     return $this->db->get()->result();
   }
 
